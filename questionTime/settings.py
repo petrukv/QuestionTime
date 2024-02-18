3b1be27cdb6eb8e5bd552b71364df68eb0ea35c6
Vadym <petrukvadd@gmail.com>
2024-02-18 19:51:35 +0200

    Core and questions apps

--- questionTime/settings.py
+++ questionTime/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users',
+    'questions',
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
---
+++ questions/models.py
@@ -0,0 +1,25 @@
+import uuid as uuid_lib
+
+from django.conf import settings
+from django.db import models
+
+from core.models import TimeStampedModel
+
+
+class Question(TimeStampedModel):
+    content = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='questions'),
+
+    def __str__(self):
+        return self.content
+
+class Answer(TimeStampedModel):
+    uuid = models.UUIDField(db_index=True, default=uuid_lib.uuid4, editable=False)
+    body = models.TextField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='questions'),
+    voters = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='likes')
+
+    def __str__(self):
+        return self.author.username
