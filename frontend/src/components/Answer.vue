a69f254a8ea637746ed9915c82d3b69023094ecb
Vadym <petrukvadd@gmail.com>
2024-02-24 22:41:29 +0200

    added the answer component

---
+++ frontend/src/components/Answer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p class="text-muted">
+            <strong>{{ answer.author }} &#8901; {{ answer.created_at }}</strong>
+        </p>
+        <p style="white-space: pre-wrap;">{{ answer.body }}</p>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AnswerComponent',
+    props: {
+        answer: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
--- frontend/src/views/QuestionView.vue
+++ frontend/src/views/QuestionView.vue
@@ -4,14 +4,26 @@
             <h1>{{ question.content }}</h1>
             <p class="mb-0">Posted by: <span class="author-name">{{ question.author }}</span></p>
             <p>{{ question.created_at }}</p>
+            <hr>
         </div>
         <div v-else>
             <h1 class="error text-center">404 question not found</h1>
+        </div>
+        <div v-if="question" >
+            <AnswerComponent v-for="answer in answers"
+            :key="answer.uuid"
+            :answer="answer" />
+        </div>
+
+        <div class="my-4">
+            <p v-show="loadingAnswers">...loading...</p>
+            <button v-show="next" @click="getQuestionAnswers" class="btn btn-sm btn-outline-success">Load More</button>
         </div>
     </div>
 </template>
 
 <script>
+import AnswerComponent from '@/components/Answer.vue';
 import axios from 'axios';
 
 export default {
@@ -22,9 +34,17 @@ export default {
             required: true,
         },
     },
+
+    components:{
+        AnswerComponent,
+    },
+
     data() {
         return {
-            question: null
+            question: null,
+            answers: [],
+            next: null,
+            loadingAnswers: false,
         };
     },
     methods: {
@@ -47,10 +67,39 @@ export default {
                 const title = '404 - Not Found'
                 this.setPageTitle(title)
             }
+        },
+
+        async getQuestionAnswers() {
+            let endpoint = `http://localhost:8000/api/v1/questions/${this.slug}/answers/`;
+            if (this.next) {
+                endpoint = this.next
+            }
+            this.loadingAnswers = true;
+            try {
+                const response = await axios.get(endpoint, {
+                headers: {
+                    'Authorization': 'token 44878bbd1715cd56ffe65543f775be71e7bf0635'
+                    }
+                });
+
+                this.answers.push(...response.data.results)
+                console.log(this.answers)
+                this.loadingAnswers = false;
+
+                if(response.data.next) {
+                    this.next = response.data.next;
+                } else {
+                    this.next = null;
+                }
+            } catch(error) {
+                console.log(error.response);
+                alert(error.response.statusText);
+            }
         }
     },
     created() {
         this.getQuestionData();
+        this.getQuestionAnswers()
     }
 }
 </script>
